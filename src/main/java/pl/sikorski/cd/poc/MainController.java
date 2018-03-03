0d9aeba865c6de8cdb5545cc9976a786fc86be40
Maciej Sikorski <maciej.sikorski@allegro.pl>
2018-03-03 12:09:45 +0100

    new message

--- src/main/java/pl/sikorski/cd/poc/MainController.java
+++ src/main/java/pl/sikorski/cd/poc/MainController.java
@@ -10,7 +10,7 @@ public class MainController {
 
     @GetMapping
     public String hello() {
-        return "Hello!";
+        return "Hello from brand new deployment!";
     }
 
     @GetMapping("/v2")
